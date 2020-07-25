4282ce5bd78d6b29ce36695ac631fed4cda62bee
Mochamad Aufa <mochamadaufa7@gmail.com>
2020-07-26 00:02:55 +0700

    [25 July 2020] add sb-admin and managing 'resources/views'

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,100 +1,57 @@
 <!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
+<html lang="en">
 
-        <title>Laravel</title>
+<head>
 
-        <!-- Fonts -->
-        <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <meta name="description" content="">
+  <meta name="author" content="">
 
-        <!-- Styles -->
-        <style>
-            html, body {
-                background-color: #fff;
-                color: #636b6f;
-                font-family: 'Nunito', sans-serif;
-                font-weight: 200;
-                height: 100vh;
-                margin: 0;
-            }
+  <title>SB Admin 2 - Dashboard</title>
 
-            .full-height {
-                height: 100vh;
-            }
+  <!-- Custom fonts for this template-->
+  <script src="https://kit.fontawesome.com/2c968ed82e.js" crossorigin="anonymous"></script>
+  <link href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i" rel="stylesheet">
 
-            .flex-center {
-                align-items: center;
-                display: flex;
-                justify-content: center;
-            }
+  <!-- Custom styles for this template-->
+  <link href="{{ URL::asset('sbadmin/css/sb-admin-2.min.css') }}" rel="stylesheet">
+  <link href="{{ URL::asset('sbadmin/vendor/datatables/dataTables.bootstrap4.min.css') }}" rel="stylesheet">
 
-            .position-ref {
-                position: relative;
-            }
+</head>
 
-            .top-right {
-                position: absolute;
-                right: 10px;
-                top: 18px;
-            }
+<body id="page-top">
+  <div id="wrapper">
+    <x-sidebar />
+    <div id="content-wrapper" class="d-flex flex-column">
+      <div id="content">
+        <x-navbar /> 
+        @yield('continer')
+      </div>
+      <x-footer />
+    </div>
+  </div>
 
-            .content {
-                text-align: center;
-            }
+  <a class="scroll-to-top rounded" href="#page-top">
+    <i class="fas fa-angle-up"></i>
+  </a>
+  <x-modal-logout/>
 
-            .title {
-                font-size: 84px;
-            }
+  <!-- Bootstrap core JavaScript-->
+  <script src="{{ URL::asset('sbadmin/vendor/jquery/jquery.min.js') }}"></script>
+  <script src="{{ URL::asset('sbadmin/vendor/bootstrap/js/bootstrap.bundle.min.js') }}"></script>
+  <!-- Core plugin JavaScript-->
+  <script src="{{ URL::asset('sbadmin/vendor/jquery-easing/jquery.easing.min.js') }}"></script>
+  <!-- Custom scripts for all pages-->
+  <script src="{{ URL::asset('sbadmin/js/sb-admin-2.min.js') }}"></script>
+  <!-- Page level plugins -->
+  <script src="{{ URL::asset('sbadmin/vendor/datatables/jquery.dataTables.min.js') }}"></script>
+  <script src="{{ URL::asset('sbadmin/vendor/datatables/dataTables.bootstrap4.min.js') }}"></script>
+  <script src="{{ URL::asset('sbadmin/vendor/chart.js/Chart.min.js') }}"></script>
+  <!-- Page level custom scripts -->
+  <script src="{{ URL::asset('sbadmin/js/demo/chart-area-demo.js') }}"></script>
+  <script src="{{ URL::asset('sbadmin/js/demo/chart-pie-demo.js') }}"></script>
+</body>
 
-            .links > a {
-                color: #636b6f;
-                padding: 0 25px;
-                font-size: 13px;
-                font-weight: 600;
-                letter-spacing: .1rem;
-                text-decoration: none;
-                text-transform: uppercase;
-            }
-
-            .m-b-md {
-                margin-bottom: 30px;
-            }
-        </style>
-    </head>
-    <body>
-        <div class="flex-center position-ref full-height">
-            @if (Route::has('login'))
-                <div class="top-right links">
-                    @auth
-                        <a href="{{ url('/home') }}">Home</a>
-                    @else
-                        <a href="{{ route('login') }}">Login</a>
-
-                        @if (Route::has('register'))
-                            <a href="{{ route('register') }}">Register</a>
-                        @endif
-                    @endauth
-                </div>
-            @endif
-
-            <div class="content">
-                <div class="title m-b-md">
-                    Laravel
-                </div>
-
-                <div class="links">
-                    <a href="https://laravel.com/docs">Docs</a>
-                    <a href="https://laracasts.com">Laracasts</a>
-                    <a href="https://laravel-news.com">News</a>
-                    <a href="https://blog.laravel.com">Blog</a>
-                    <a href="https://nova.laravel.com">Nova</a>
-                    <a href="https://forge.laravel.com">Forge</a>
-                    <a href="https://vapor.laravel.com">Vapor</a>
-                    <a href="https://github.com/laravel/laravel">GitHub</a>
-                </div>
-            </div>
-        </div>
-    </body>
 </html>
